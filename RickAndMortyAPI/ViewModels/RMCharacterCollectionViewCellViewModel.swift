435f8dafed7228b807f3082fb1f9b1e4fbe92e6f
minckim1222 <min.c.kim@gmail.com>
2023-10-20 15:43:17 -0500

    Created ViewModel for CharacterCollectionView cell.

--- RickAndMortyAPI/ViewModels/RMCharacterCollectionViewCellViewModel.swift
+++ RickAndMortyAPI/ViewModels/RMCharacterCollectionViewCellViewModel.swift
@@ -10,4 +10,24 @@ import Foundation
 /// ViewModel for configuring CollectionView cell
 struct RMCharacterCollectionViewCellViewModel {
     
+    let characterName: String
+    let characterStatus: RMCharacterStatus
+    let characterImageUrl: URL?
+    
+    /// Public init
+    /// - Parameters:
+    ///   - characterName: Name of character
+    ///   - characterStatus: Status of character
+    ///   - characterImageUrl: URL for the character's image
+    init(characterName: String, characterStatus: RMCharacterStatus, characterImageUrl: URL?) {
+        self.characterName = characterName
+        self.characterStatus = characterStatus
+        self.characterImageUrl = characterImageUrl
+    }
+    
+    /// Computed property to get the character's status
+    var characterStatusText: String {
+        return characterStatus.rawValue
+    }
+    
 }
--- RickAndMortyAPI/Views/RMCharacterCollectionViewCell.swift
+++ RickAndMortyAPI/Views/RMCharacterCollectionViewCell.swift
@@ -11,19 +11,55 @@ import UIKit
 class RMCharacterCollectionViewCell: UICollectionViewCell {
     static let reuseIdentifier = "RMCharacterCollectionViewCell"
     
+    /// ImageView to hold the Character image
+    private let characterImageView: UIImageView = {
+        
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    /// Label to hold the character name
+    private let nameLabel: UILabel = {
+        
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 18, weight: .medium)
+        label.textColor = .label
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    /// Label to hold the character status
+    private let statusLabel: UILabel = {
+        
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 16, weight: .regular)
+        label.textColor = .label
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         contentView.backgroundColor = .secondarySystemBackground
+        contentView.addSubviews(characterImageView, nameLabel, statusLabel)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    /// Empty out our cell before reusing
     override func prepareForReuse() {
         super.prepareForReuse()
+        characterImageView.image = nil
+        nameLabel.text = nil
+        statusLabel.text = nil
     }
     
+    /// Function that configures our CollectionViewCell
+    /// - Parameter with: ViewModel for our cell configuration
     public func configure(with: RMCharacterCollectionViewCellViewModel) {
         
     }
